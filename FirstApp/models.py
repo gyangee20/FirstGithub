e7066cab7daeff1a049504d00ea64d35dd083447
GP <gyangee.20@gmail.com>
2018-04-29 20:42:00 +0530

    upadte on models.py

--- FirstApp/models.py
+++ FirstApp/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-class Menu(models.Model):
-    menuId=models.IntegerField(primary_key=True)
-    name=models.CharField(max_length=20)
-class Item(models.Model):
-    itemId=models.IntegerField(primary_key=True)
-    name=models.CharField(max_length=20)
-    print=models.DecimalField(max_digits=5, decimal_places=2)
