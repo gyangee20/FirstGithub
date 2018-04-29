412ee301853352413f64e6b08b6a4382030af539
GP <gyangee.20@gmail.com>
2018-04-29 20:48:55 +0530

    upadte on models.py

--- FirstApp/models.py
+++ FirstApp/models.py
@@ -6,3 +6,4 @@ class Item(models.Model):
     itemId=models.IntegerField(primary_key=True)
     name=models.CharField(max_length=20)
     price=models.DecimalField(max_digits=5,decimal_places=2)
+    menu=models.ForeignKey(Menu,on_delete=models.CASCADE)
